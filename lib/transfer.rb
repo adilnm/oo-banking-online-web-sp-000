9a14644e6c8e1256963c5f0879a711b7a7b05815
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-20 07:37:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,9 @@
 class Transfer
-     attr_accessor :sender,:receiver,:amount
+     attr_accessor :sender,:receiver,:amount,
   def initialize(sender,receiver,amount)
     @sender=sender
     @receiver=receiver
     @amount=amount
+    @status="pending"
   end
 end
